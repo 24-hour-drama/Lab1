73f11f6eb96a8c9f2f46eb2f5bca937573ec2b40
dibko <dibko@DESKTOP-T71V041>
2026-09-09 00:14:21 +0300

    Создание класса статов и начало класса контроля

---
+++ Library/NightlightController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    internal class NightlightController
+    {
+        private NightlightState State { get; set; }
+        int newBrightness;
+        public void SetBrightness()
+        {
+            if (State.isOn == true && (newBrightness <= 100 && newBrightness >= 0))
+            {
+                State.Brightness = newBrightness;
+            }
+        }
+    }
+}
---
+++ Library/NightlightState.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Library
+{
+    public class NightlightState
+    {
+        public bool isOn;
+        public int Brightness;
+        public Color currentColor; // если буде делать цветным
+        public int SensorLight; // если будем делать сеенсор освещения вокруг
+        public string currentMod;
+    }
+}
