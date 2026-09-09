52e62e64515ebdafe2f7ab7227b1e68661539866
dibko <dibko@DESKTOP-T71V041>
2026-09-09 17:45:38 +0300

    Классы с комментариями

--- Library/NightlightState.cs
+++ Library/NightlightState.cs
@@ -1,13 +1,17 @@
 ﻿using System.Drawing;
+using System.Xml;
 
 namespace Library
 {
+    /// <summary>
+    /// Состояние ночника
+    /// </summary>
     public class NightlightState
     {
-        public bool isOn;
-        public int Brightness;
-        public Color currentColor; // если буде делать цветным
-        public int SensorLight; // если будем делать сеенсор освещения вокруг
-        public string currentMod;
+        public bool isOn = false;
+        public int Brightness = 50;
+        public string currentColor = "white"; // если буде делать цветным
+        public int TimerMinutes = 0;
+        public bool isTimerActive = false;
     }
 }
